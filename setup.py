ef9aefeed96147027018e3026a973aea180844bb
Filippo Romani <mail@filipporomani.it>
2023-04-08 12:20:34 +0200

    new version coming!

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, "README.md")) as f:
 
 setup(
     name="whatsapp-python",
-    version="1.0.2",
+    version="1.1.2",
     description="Opensource Python wrapper to WhatsApp Cloud API",
     long_description=long_description,
     long_description_content_type="text/markdown",
