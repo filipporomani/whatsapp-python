5461e7a9ac5852203b05d63770e515f15a2e6007
Filippo Romani <mail@filipporomani.it>
2024-06-11 13:42:00 +0200

    add comments

--- whatsapp/constants.py
+++ whatsapp/constants.py
@@ -1,3 +1,4 @@
-# internal use only - numbers and . only 
+# This file contains the constants used in the project.
+# The VERSION constant is used to store the version of the project - it's not only used in the __init__.py file, but also in the pyproject.toml file.
 
 VERSION = "3.1.1"
