93117c989104d7924fbacf8c04f5d82692c2294b
Filippo Romani <filippo@romani.cc>
2024-10-02 19:13:47 +0200

    update version (3.3.9.post2)

--- whatsapp/constants.py
+++ whatsapp/constants.py
@@ -1,4 +1,4 @@
 # This file contains the constants used in the project.
 # The VERSION constant is used to store the version of the project - it's not only used in the __init__.py file, but also in the pyproject.toml file.
 
-VERSION = "3.3.9.post1"
+VERSION = "3.3.9.post2"
