9fe3127cefe4d8a4df017cca6f63f011fc2929f2
Filippo Romani <filippo@romani.cc>
2024-11-08 15:33:10 +0100

    create AsyncWhatsapp object

--- whatsapp/__init__.py
+++ whatsapp/__init__.py
@@ -4,6 +4,8 @@ Unofficial Python wrapper for the WhatsApp Cloud API.
 from __future__ import annotations
 import requests
 import logging
+import json
+import aiohttp
 from bs4 import BeautifulSoup
 from fastapi import FastAPI, HTTPException, Request
 from uvicorn import run as _run
@@ -15,8 +17,6 @@ from .ext._send_media import send_image, send_video, send_audio, send_location, 
 from .ext._media import upload_media, query_media_url, download_media, delete_media
 from .ext._buttons import send_button, create_button, send_reply_button
 from .ext._static import is_message, get_mobile, get_author, get_name, get_message, get_message_id, get_message_type, get_message_timestamp, get_audio, get_delivery, get_document, get_image, get_sticker, get_interactive_response, get_location, get_video, changed_field
-import json
-
 
 class WhatsApp(object):
     def __init__(self, token: str = "", phone_number_id: dict = "", logger: bool = True, update_check: bool = True, verify_token: str = "", debug: bool = True, version: str = "latest"):
@@ -132,8 +132,8 @@ class WhatsApp(object):
             if r.query_params.get("hub.verify_token") == self.verify_token:
                 logging.debug("Webhook verified successfully")
                 challenge = r.query_params.get("hub.challenge")
-                self.verification_handler(challenge)
-                self.other_handler(challenge)
+                await self.verification_handler(challenge)
+                await self.other_handler(challenge)
                 return int(challenge)
             logging.error("Webhook Verification failed - token mismatch")
             await self.verification_handler(False)
@@ -168,43 +168,45 @@ class WhatsApp(object):
 
     # all the files starting with _ are imported here, and should not be imported directly.
 
-    is_message = staticmethod(is_message)
-    get_mobile = staticmethod(get_mobile)
-    get_name = staticmethod(get_name)
-    get_message = staticmethod(get_message)
-    get_message_id = staticmethod(get_message_id)
-    get_message_type = staticmethod(get_message_type)
-    get_message_timestamp = staticmethod(get_message_timestamp)
-    get_audio = staticmethod(get_audio)
-    get_delivery = staticmethod(get_delivery)
-    get_document = staticmethod(get_document)
-    get_image = staticmethod(get_image)
-    get_sticker = staticmethod(get_sticker)
-    get_interactive_response = staticmethod(get_interactive_response)
-    get_location = staticmethod(get_location)
-    get_video = staticmethod(get_video)
-    changed_field = staticmethod(changed_field)
-    get_author = staticmethod(get_author)
+    def import_all():
+        is_message = staticmethod(is_message)
+        get_mobile = staticmethod(get_mobile)
+        get_name = staticmethod(get_name)
+        get_message = staticmethod(get_message)
+        get_message_id = staticmethod(get_message_id)
+        get_message_type = staticmethod(get_message_type)
+        get_message_timestamp = staticmethod(get_message_timestamp)
+        get_audio = staticmethod(get_audio)
+        get_delivery = staticmethod(get_delivery)
+        get_document = staticmethod(get_document)
+        get_image = staticmethod(get_image)
+        get_sticker = staticmethod(get_sticker)
+        get_interactive_response = staticmethod(get_interactive_response)
+        get_location = staticmethod(get_location)
+        get_video = staticmethod(get_video)
+        changed_field = staticmethod(changed_field)
+        get_author = staticmethod(get_author)
 
-    send_button = send_button
-    create_button = create_button
-    send_reply_button = send_reply_button
-    send_image = send_image
-    send_video = send_video
-    send_audio = send_audio
-    send_location = send_location
-    send_sticker = send_sticker
-    send_document = send_document
-    upload_media = upload_media
-    query_media_url = query_media_url
-    download_media = download_media
-    delete_media = delete_media
-    send_template = send_template
-    send_custom_json = send_custom_json
-    send_contacts = send_contacts
+        send_button = send_button
+        create_button = create_button
+        send_reply_button = send_reply_button
+        send_image = send_image
+        send_video = send_video
+        send_audio = send_audio
+        send_location = send_location
+        send_sticker = send_sticker
+        send_document = send_document
+        upload_media = upload_media
+        query_media_url = query_media_url
+        download_media = download_media
+        delete_media = delete_media
+        send_template = send_template
+        send_custom_json = send_custom_json
+        send_contacts = send_contacts
+        authorized = property(authorized)
 
-    authorized = property(authorized)
-
+    import_all()
+    
     def create_message(self, **kwargs) -> Message:
         """
         Create a message object
@@ -246,6 +248,239 @@ class WhatsApp(object):
 
     def run(self, host: str = "localhost", port: int = 5000, **options):
         _run(self.app, host=host, port=port, **options)
+
+
+class AsyncWhatsApp(WhatsApp):
+    def __init__(self, token: str = "", phone_number_id: dict = "", logger: bool = True, update_check: bool = True, verify_token: str = "", debug: bool = True, version: str = "latest"):
+        """
+        Initialize the WhatsApp Object
+
+        Args:
+            token[str]: Token for the WhatsApp cloud API obtained from the Facebook developer portal
+            phone_number_id[str]: Phone number id for the WhatsApp cloud API obtained from the developer portal
+            logger[bool]: Whether to enable logging or not (default: True)
+        """
+
+        # Check if the version is up to date
+        logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+        self.l = phone_number_id
+
+        if isinstance(phone_number_id, dict):
+            # use first phone number id as default
+            phone_number_id = phone_number_id[list(phone_number_id.keys())[0]]
+
+        elif phone_number_id == "":
+            logging.error("Phone number ID not provided")
+            raise ValueError(
+                "Phone number ID not provided but required")
+        elif isinstance(phone_number_id, str):
+            logging.critical(
+                "The phone number ID should be a dictionary of phone numbers and their IDs. Using strings is deprecated.")
+            raise ValueError(
+                "Phone number ID should be a dictionary of phone numbers and their IDs")
+        else:
+            pass
+
+        self.VERSION = VERSION  # package version
+        r = requests.get("https://developers.facebook.com/docs/graph-api/changelog/").text
+
+        # dynamically get the latest version of the API
+        if version == "latest":
+            soup = BeautifulSoup(r, features="html.parser")
+            t1 = soup.findAll("table")
+
+            def makeversion(table):
+                result = []
+                allrows = table.findAll('tr')
+                for row in allrows:
+                    result.append([])
+                    allcols = row.findAll('td')
+                    for col in allcols:
+                        thestrings = [(s) for s in col.findAll(text=True)]
+                        thetext = ''.join(thestrings)
+                        result[-1].append(thetext)
+                return result[0][1]
+            self.LATEST = makeversion(t1[0])  # latest version of the API
+        else:
+            self.LATEST = version
+
+        if update_check is True:
+            latest = str(requests.get(
+                "https://pypi.org/pypi/whatsapp-python/json").json()["info"]["version"])
+            if self.VERSION != latest:
+                try:
+                    version_int = int(self.VERSION.replace(".", ""))
+                except:
+                    version_int = 0
+                try:
+                    latest_int = int(latest.replace(".", ""))
+                except:
+                    latest_int = 0
+                # this is to avoid the case where the version is 1.0.10 and the latest is 1.0.2 (possible if user is using the github version)
+                if version_int < latest_int:
+                    if version_int == 0:
+                        logging.critical(
+                            f"There was an error while checking for updates, please check for updates manually. This may be due to the version being a post-release version (e.g. 1.0.0.post1) or a pre-release version (e.g. 1.0.0a1). READ THE CHANGELOG BEFORE UPDATING. NEW VERSIONS MAY BREAK YOUR CODE IF NOT PROPERLY UPDATED.")
+                    else:
+                        logging.critical(
+                            f"Whatsapp-python is out of date. Please update to the latest version {latest}. READ THE CHANGELOG BEFORE UPDATING. NEW VERSIONS MAY BREAK YOUR CODE IF NOT PROPERLY UPDATED.")
+
+        if token == "":
+            logging.error("Token not provided")
+            raise ValueError("Token not provided but required")
+        if phone_number_id == "":
+            logging.error("Phone number ID not provided")
+            raise ValueError(
+                "Phone number ID not provided but required")
+        self.token = token
+        self.phone_number_id = phone_number_id
+        self.base_url = f"https://graph.facebook.com/{self.LATEST}"
+        self.url = f"{self.base_url}/{phone_number_id}/messages"
+        self.verify_token = verify_token
+
+        async def base(*args):
+            pass
+        self.message_handler = base
+        self.other_handler = base
+        self.verification_handler = base
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token}"
+        }
+        if logger is False:
+            logging.disable(logging.INFO)
+            logging.disable(logging.ERROR)
+        if debug is False:
+            logging.disable(logging.DEBUG)
+            logging.disable(logging.ERROR)
+
+        self.app = FastAPI()
+
+        # Verification handler has 1 argument: challenge (str | bool): str if verification is successful, False if not
+
+        @self.app.get("/")
+        async def verify_endpoint(r: Request):
+            if r.query_params.get("hub.verify_token") == self.verify_token:
+                logging.debug("Webhook verified successfully")
+                challenge = r.query_params.get("hub.challenge")
+                await self.verification_handler(challenge)
+                await self.other_handler(challenge)
+                return int(challenge)
+            logging.error("Webhook Verification failed - token mismatch")
+            await self.verification_handler(False)
+            await self.other_handler(False)
+            return {"success": False}
+
+        @self.app.post("/")
+        async def hook(r: Request):
+            try:
+                # Handle Webhook Subscriptions
+                data = await r.json()
+                if data is None:
+                    return {"success": False}
+                data_str = json.dumps(data, indent=4)
+                # log the data received only if the log level is debug
+                logging.debug(f"Received webhook data: {data_str}")
+
+                changed_field = self.changed_field(data)
+                if changed_field == "messages":
+                    new_message = self.is_message(data)
+                    if new_message:
+                        msg = Message(instance=self, data=data)
+                        await self.message_handler(msg)
+                        await self.other_handler(msg)
+                return {"success": True}
+            except Exception as e:
+                logging.error(f"Error parsing message: {e}")
+                raise HTTPException(status_code=500, detail={
+                    "success": False,
+                    "error": str(e)
+                })
+
+    # all the files starting with _ are imported here, and should not be imported directly.
+
+    def import_all():
+        is_message = staticmethod(is_message)
+        get_mobile = staticmethod(get_mobile)
+        get_name = staticmethod(get_name)
+        get_message = staticmethod(get_message)
+        get_message_id = staticmethod(get_message_id)
+        get_message_type = staticmethod(get_message_type)
+        get_message_timestamp = staticmethod(get_message_timestamp)
+        get_audio = staticmethod(get_audio)
+        get_delivery = staticmethod(get_delivery)
+        get_document = staticmethod(get_document)
+        get_image = staticmethod(get_image)
+        get_sticker = staticmethod(get_sticker)
+        get_interactive_response = staticmethod(get_interactive_response)
+        get_location = staticmethod(get_location)
+        get_video = staticmethod(get_video)
+        changed_field = staticmethod(changed_field)
+        get_author = staticmethod(get_author)
+
+        send_button = send_button
+        create_button = create_button
+        send_reply_button = send_reply_button
+        send_image = send_image
+        send_video = send_video
+        send_audio = send_audio
+        send_location = send_location
+        send_sticker = send_sticker
+        send_document = send_document
+        upload_media = upload_media
+        query_media_url = query_media_url
+        download_media = download_media
+        delete_media = delete_media
+        send_template = send_template
+        send_custom_json = send_custom_json
+        send_contacts = send_contacts
+        authorized = property(authorized)
+
+    import_all()
+    
+    def create_message(self, **kwargs) -> Message:
+        """
+        Create a message object
+
+        Args:
+            data[dict]: The message data
+            content[str]: The message content
+            to[str]: The recipient
+            rec_type[str]: The recipient type (individual/group)
+        """
+        return Message(**kwargs, instance=self)
+
+    def on_message(self, handler: function):
+        """
+        Set the handler for incoming messages
+
+        Args:
+            handler[function]: The handler function
+        """
+        self.message_handler = handler
+
+    def on_event(self, handler: function):
+        """
+        Set the handler for other events
+
+        Args:
+            handler[function]: The handler function
+        """
+        self.other_handler = handler
+
+    def on_verification(self, handler: function):
+        """
+        Set the handler for verification
+
+        Args:
+            handler[function]: The handler function
+        """
+        self.verification_handler = handler
+
+    def run(self, host: str = "localhost", port: int = 5000, **options):
+        _run(self.app, host=host, port=port, **options)
+
 
 
 class Message(object):
